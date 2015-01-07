d2ab4afd95c0041dbf0af13ad5eedf1f20ca875f
Morketh <admmorketh@gmail.com>
2015-01-07 01:38:37 -0700

    dynmap fullrender support
    this sets up an exclude for dynmap in order to save bandwidth and reduce
    sync time also tells the server to generate a fullrender of the map
    AFTER we sync

--- mc-back.sh
+++ mc-back.sh
@@ -5,6 +5,9 @@ BNAME="CI_[1-7-10]_Cauldron_1-1207-01-198"
 REMOTE_USER=admiral
 REMOTE_HOST=uesfrp.no-ip.org
 
+## this will provide the world name to the the dynmap engine for a full render
+MAP_NAME=CI_LVL_3
+
 ## SYSTEM VARIABLES DO NOT CHANGE ##
 NOW=$(date +"%m-%d-%Y")
 FILE="$BNAME.$(date +"%m-%d-%Y").tar.gz"
@@ -29,12 +32,17 @@ done
 echo "[$(date +"%m-%d-%Y") $(date +"%H%M")] Server PID $MCPID died.. running backup" >> $LOG_FILE
 
 ssh $REMOTE_USER@$REMOTE_HOST "screen -p 0 -S Cauldron_1-7-10 -X stuff 'say $(date +"%H%M") MST [$(uname -n)] preforming an incremental backup of the world please stand-by$(printf \\r)'"
-rsync -avzhe ssh $REMOTE_USER@$REMOTE_HOST:/opt/Cauldron_1-7-10_1-1207-01-198 /opt
+## Adds an --exclude for the dynmap folder >..< saves bandwidth as we have to re render the map on our side anyway
+## this also cuts sync time to almost 1 minute ^_^
+rsync -avzhe ssh --exclude 'dynmap/*' $REMOTE_USER@$REMOTE_HOST:/opt/Cauldron_1-7-10_1-1207-01-198 /opt
 
 echo "[$(date +"%m-%d-%Y") $(date +"%H%M")] Minecraft systems synced" >> $LOG_FILE
 ssh $REMOTE_USER@$REMOTE_HOST "screen -p 0 -S Cauldron_1-7-10 -X stuff 'say $(date +"%H%M") MST [$(uname -n)] Systems Synced. $(uname -n) restarting.$(printf \\r)'"
 ## turn local MC back online ##
 screen -d -m -S SCREEN_Cauldron_1-7-10 bash -c 'java -Xmx2G -Xms1G -XX:MaxPermSize=1024m -XX:+CMSClassUnloadingEnabled -jar cauldron-1.7.10-1.1207.01.198-server.jar'
+
+## Send full render to the server
+screen -p 0 -S SCREEN_Cauldron_1-7-10 -X stuff "dynmap fullrender $MAP_NAME$(printf \\r)"
 
 ### MODE PUSH ###
 ## TODO add in code to push a local copy out to the remote host
