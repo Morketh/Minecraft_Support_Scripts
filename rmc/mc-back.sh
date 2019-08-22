1150dcbdaf513d3b9742f788ecbc36ebff3b22ea
Morketh <admmorketh@gmail.com>
2019-08-21 22:59:21 -0500

    modifies mc-back to provide remote/local options for MC server + backups

--- rmc/mc-back.sh
+++ rmc/mc-back.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 ## User provided Variables ##
-REMOTE_USER=user
-REMOTE_HOST=host
-SSH_PORT=22
-SCREEN_NAME=1-7-10_forge
 
-LOG_FILE=/var/log/mc-back.log
+# Server name is typically the name of the root minecraft server directory
 TIME_ZONE=":US/Pacific"
+SERVER_NAME=minecraft-1.12.2
+
+# Backup Related Settings
+EXCLUDE_FROM=/opt/etc/exclude.conf
+PATH_FROM=/opt/minecraft
+PATH_TO=/opt/minecraft
+
+# Remote MC Server?
+REMOTE_SERVER=false
+
+# Remote Backup?
+REMOTE_BACKUP=true
+
+# if REMOTE_SERVER or REMOTE_BAKCUP = True these settings are enabled and used
+# File transfers are rsynced using an SSH Connection
+# REQUIRED: SSH keypair for unatended user access
+REMOTE_USER=user
+REMOTE_HOST=host.example.com
+SSH_PORT=22
+
+# Log file location for backup process
+LOG_FILE=/var/log/mc-back.log
 
 ## SYSTEM VARIABLES DO NOT CHANGE ##
 NOW=$(date +"%m-%d-%Y")
@@ -17,15 +35,22 @@ NOW=$(date +"%m-%d-%Y")
 function MCCommunicate()
 {
     # Dont forget to set NAME or whatever
-    ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST screen -S $SCREEN_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    if [ "$REMOTE_SERVER" = true ]; then
+        ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST screen -S $SCREEN_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    else
+        screen -S $SERVER_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    fi
 }
 
 ## start  ##
 echo "[$NOW $(date +"%H:%M")] Minecraft Backup started" >> $LOG_FILE
-MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M")] [$(uname -n)] Performing an incremental backup of the world."
+MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M") US-Pac] [$(uname -n)] Performing an incremental backup of the world."
 
 # During sync delete removed files so our copy is identical to the master, also ignore logs and remove them from our local copy.
-rsync -avzh --delete-during --delete-excluded --exclude-from=/opt/etc/exclude.conf -e "ssh -p $SSH_PORT" $REMOTE_USER@$REMOTE_HOST:/opt/minecraft /opt/ >> $LOG_FILE
-
+if [ "$REMOTE_BACKUP" = true ]; then
+    rsync -avh --compress-level=10 --itemize-changes --sparse --delete-during --delete-excluded --exclude-from=$EXCLUDE_FROM -e "ssh -p $SSH_PORT" $REMOTE_USER@$REMOTE_HOST:$PATH_FROM/$SERVER_NAME/ $PATH_TO/$SERVER_NAME >> $LOG_FILE
+else
+    rsync -avh --compress-level=10 --itemize-changes --sparse --delete-during --delete-excluded --exclude-from=$EXCLUDE_FROM  $PATH_FROM/$SERVER_NAME/ $PATH_TO/$SERVER_NAME >> $LOG_FILE
+fi
 echo "[$NOW $(date +"%H:%M")] Minecraft systems synced" >> $LOG_FILE
-MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M")] [$(uname -n)] Systems synced."
+MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M") US-Pac] [$(uname -n)] Systems synced."
