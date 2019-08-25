155aec71ef71d68e49daa3fb56f1c1a48874cf18
Morketh <admmorketh@gmail.com>
2019-08-25 14:20:06 -0500

    Updates settings and changes restart scripts for newer version of MC.

--- rmc/mc-restart.sh
+++ rmc/mc-restart.sh
@@ -1,44 +1,60 @@
 #!/bin/bash
 
 ## User provided Variables ##
-REMOTE_USER=user
-REMOTE_HOST=host
-SSH_PORT=22
-SCREEN_NAME=1-7-10_forge
-LOG_FILE=/var/log/mc-back.log
+
+# Server name is typically the name of the root minecraft server directory
 TIME_ZONE=":US/Pacific"
+SERVER_NAME=minecraft-1.12.2
+
+# Remote MC Server?
+REMOTE_SERVER=false
+
+# if REMOTE_SERVER = True these settings are enabled and used
+# REQUIRED: SSH keypair for unatended user access
+REMOTE_USER=user
+REMOTE_HOST=host.example.com
+SSH_PORT=22
 
 ## SYSTEM VARIABLES DO NOT CHANGE ##
 NOW=$(date +"%m-%d-%Y")
+PID=$(cat server.pid)
 
 ## Functions ##
 
 function MCCommunicate()
 {
     # Dont forget to set NAME or whatever
-    ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST screen -S $SCREEN_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    if [ "$REMOTE_SERVER" = true ]; then
+        ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST screen -S $SCREEN_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    else
+        screen -S $SERVER_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    fi
+}
+
+function MCReboot()
+{
+    if [ "$REMOTE_SERVER" = true ]; then
+        ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST screen -S $SCREEN_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    else
+        screen -S $SERVER_NAME -p 0 -X stuff \"$1$(printf \\r)\"
+    fi
 }
 
 ## start  ##
 MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M")] [$(uname -n)] Server restart in 10 minutes. Reason: $1"
 sleep 5m # 5 minute warrnings.
 
-MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M")] [$(uname -n)] Server restart in 5 minutes."
+MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M")] [$(uname -n)] Server restart in 5 minutes. Reason: $1"
 
 sleep 4m
 
 MCCommunicate "say [$(TZ=$TIME_ZONE date +"%H:%M")] [$(uname -n)] Server restart in 1 minute. There will be no further warnings anyone left on the server will be kicked. Please allow 2 to 5 minutes for server startup."
 
-
 sleep 1m
 
-MCCommunicate "save-off"
 MCCommunicate "save-all"
 MCCommunicate "backup"
 
-sleep 10s
+sleep 30s
 
-# Once the backups and saves are done wait a little bit then kill the server and restart it
 MCCommunicate "stop"
-
-ssh -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST "cd /opt/minecraft; ./mcstart.bash" >> $LOG_FILE
--- rmc/mcstart.bash
+++ rmc/mcstart.bash
@@ -3,11 +3,12 @@ LOG_FILE=/var/log/minecraft/server.log
 MAX_MEMORY=10G
 MIN_MEMORY=2G
 MAX_PERM_SIZE=4G
+SERVER_NAME=PixelmonRails
 
 TIME_ZONE=":US/Pacific"
 
 ## System variables DO NOT EDIT ##
-JAR=forge-1.7.10-10.13.4.1566-1.7.10-universal.jar
+JAR=forge-1.12.2-14.23.5.2838-universal.jar
 NOW=$(date +"%m-%d-%Y %H:%M")
 PID=$(cat server.pid)
 
@@ -21,14 +22,13 @@ done
 echo "[$NOW] Minecraft Server stopped... Checking for newer release of forge essentials." >> $LOG_FILE
 
 # Redirect output to log file.
-wget -N http://files.forgeessentials.com/forgeessentials-1.7.10-server.jar -P mods/ 2>> $LOG_FILE
+wget -N http://files.forgeessentials.com/forgeessentials-1.12.2-server.jar -P mods/
 echo "[$NOW] Forge Essentials is upto date." >> $LOG_FILE
-
 
 echo "[$NOW] Minecraft server sarting up." >> $LOG_FILE
 
 # Load the screen use the configuration file inorder to load some defualt values.
-screen -c config/screen.cfg -d -m -L -S 1-7-10_forge bash -c "java -Xmx$MAX_MEMORY -Xms$MIN_MEMORY -jar -XX:MaxPermSize=$MAX_PERM_SIZE -XX:+CMSClassUnloadingEnabled $JAR nogui"&
-
+screen -c config/screen.cfg -d -m -L -S $SERVER_NAME bash -c "java -Xmx$MAX_MEMORY -Xms$MIN_MEMORY -jar -XX:MaxPermSize=$MAX_PERM_SIZE -XX:+CMSClassUnloadingEnabled $JAR nogui"&
 echo $! > server.pid
+
 echo "[$NOW] Minecraft Server started with PID $!" >> $LOG_FILE
